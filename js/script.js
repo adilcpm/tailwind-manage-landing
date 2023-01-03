4db5ab6ad384c4db0f82e464fc11c154a555907d
Muhammed Adil <41191975+adilcpm@users.noreply.github.com>
2023-01-03 08:33:45 +0530

    section scroll animation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-const btn = document.getElementById('menu-btn')
-const nav = document.getElementById('menu')
+// Hamburger button and mobile Nav
+const btn = document.getElementById("menu-btn");
+const nav = document.getElementById("menu");
 
-btn.addEventListener('click', () => {
-    btn.classList.toggle('open')
-    nav.classList.toggle('flex')
-    nav.classList.toggle('hidden')
-})
+btn.addEventListener("click", () => {
+  btn.classList.toggle("open");
+  nav.classList.toggle("flex");
+  nav.classList.toggle("hidden");
+});
+
+// Sections Scroll Animation
+const featuresSection = document.getElementById("features");
+const testimonialSection = document.getElementById("testimonials");
+const sections = [featuresSection, testimonialSection];
+
+window.addEventListener("scroll", checkSections);
+checkSections();
+
+function checkSections() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  for (let section of sections) {
+    const sectionTopPoint = section.getBoundingClientRect().top;
+    if (sectionTopPoint < triggerBottom) {
+      section.classList.add("show");
+    }
+  }
+}
